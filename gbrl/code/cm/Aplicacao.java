eb24ca4b446f1b1eaef5d064d711bf0bbb254b01
Gabriel Silva <32930500+H4uberk@users.noreply.github.com>
2022-08-20 04:20:18 -0300

    campo minado

---
+++ gbrl/code/cm/Aplicacao.java
@@ -0,0 +1,24 @@
+package gbrl.code.cm;
+
+import gbrl.code.cm.modelo.Tabuleiro;
+import gbrl.code.cm.visao.TabuleiroConsole;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		
+		new TabuleiroConsole(tabuleiro);
+		/*
+		tabuleiro.abrir(3, 3);
+		
+		tabuleiro.alternarMarcacao(4, 4);
+		tabuleiro.alternarMarcacao(4, 5);
+		
+		System.out.println(tabuleiro);
+		*/
+
+	}
+
+}
---
+++ gbrl/code/cm/excecao/SairException.java
@@ -0,0 +1,7 @@
+package gbrl.code.cm.excecao;
+
+public class SairException extends RuntimeException{
+
+	private static final long serialVersionUID = 1;
+
+}
